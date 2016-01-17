e4f898c8aaeacd595f4fc91d417da85e8ff2d960
sallypikachu <sallypikachu@gmail.com>
2016-01-17 18:52:33 -0500

    User model testing complete

--- spec/features/seed_data_displayed_spec.rb
+++ spec/features/seed_data_displayed_spec.rb
@@ -1,9 +1,12 @@
 require "rails_helper"
 
+# As an unauthenticated user
+# I can see the tables/maps of biodiversity/indicators of human activity
+
 # Acceptance Criteria:
 # [ ] When I first go to the home page, a table/chart/map is displayed
 # [ ] I can click on different tabs/buttons that will then display
-# different things
+#     different things
 
 feature "visit home page to see tables/charts/maps" do
   scenario "visit home page to see the biodiversity map" do
