f74de0d49d6817c80a3ccf99d6110825295544bc
sallypikachu <sallypikachu@gmail.com>
2016-01-18 14:10:27 -0500

    Clean up

--- spec/features/user_profile_spec.rb
+++ spec/features/user_profile_spec.rb
@@ -1,17 +1,12 @@
 require "rails_helper"
 
 # As an authenticated user
-# I want to sign in so that I can access the system
+# I want to see what I have contributed
 
 # Acceptance Criteria:
-# [ ] There is an option to sign in on the page if I'm not already authenticated
-# [ ] My password is not displayed as I enter it
-# [ ] I must specify a valid email address
-# [ ] If I've entered the right credentials, I receive an onscreen message that
-#     I'm signed in, and I've gained access to the system
-# [ ] If I didn't enter valid credentials, I receive an error message and I am
-#     left unauthenticated
-# [ ] If I'm already authenticated, I don't have the option to sign in
+# [ ] There is a profile option
+# [ ] I can click an edit credentials link
+
 
 feature "sees profile in top bar" do
   let!(:user) { User.create(username: "user", email: "user@email.com", password: "password") }
