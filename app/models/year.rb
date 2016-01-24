b9236939a703fc95cf3b758297cd628ec880ea6a
sallypikachu <sallypikachu@gmail.com>
2016-01-24 14:44:03 -0500

    Codeship test

--- app/models/year.rb
+++ app/models/year.rb
@@ -1,6 +1,7 @@
 class Year < ActiveRecord::Base
-  validates :year, presence: true, uniqueness: true
-  validates :year, numericality: { only_integer: true }
+  validates :year, presence: true, uniqueness: true, numericality: {
+    only_integer: true
+  }
 
   has_many :facts
   has_many :titles, through: :facts
