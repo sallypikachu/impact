2b2440808f946b2c5611265f78e5e53506a096c6
sallypikachu <sallypikachu@gmail.com>
2016-01-28 11:14:10 -0500

    pie to home

---
+++ app/assets/javascripts/world_pie.js
@@ -0,0 +1,51 @@
+$(function () {
+  $(document).ready(function () {
+      // Build the chart
+    $('#world_pie').highcharts({
+      chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+      },
+      title: {
+        text: 'Endangered Animals around the World'
+      },
+      tooltip: {
+        pointFormat: '{point.y} organisms'
+      },
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels: {
+            enabled: false
+          },
+          showInLegend: true
+        }
+      },
+      series: [{
+        name: 'Brands',
+        colorByPoint: true,
+        data: [{
+          name: 'Extinct',
+          y: 1053
+        }, {
+          name: 'Threatened',
+          y: 48379,
+          sliced: true,
+          selected: true
+        }, {
+          name: 'At Risk',
+          y: 24143
+        }, {
+          name: 'Data Deficient',
+          y: 32529
+        }, {
+          name: 'Least Concerned',
+          y: 354185
+        }]
+      }]
+    });
+  });
+});
